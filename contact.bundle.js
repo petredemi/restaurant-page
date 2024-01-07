6c241e8fd39c786fca692dbd5c8cd259c8a135ee
petrudemian <petredemi@gmail.com>
2024-01-07 20:19:13 +0000

    first commit

---
+++ contact.bundle.js
@@ -0,0 +1,131 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/contact.js":
+/*!************************!*\
+  !*** ./src/contact.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _map_jpg__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./map.jpg */ \"./src/map.jpg\");\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (contact);\n\n\nfunction contact(){\n    const contact = document.createElement('div');\n    contact.setAttribute('id', 'contact');\n    const adress = document.createElement('div');\n    adress.setAttribute('id', 'adress');\n    contact.appendChild(adress);\n\n    const adress1 = document.createElement('div');\n    const adress2 = document.createElement('div');\n    adress1.classList.add('adress');\n    adress2.classList.add('adress');\n\n    const h1 = document.createElement('h3');\n    const h2 = document.createElement('h3');\n    const h3 = document.createElement('h3');\n    \n    h1.textContent = 'Adress:';\n    h2.textContent = 'Contact:';\n    h3.textContent = 'Map:';\n\n    const p1 = document.createElement('p');\n    const p2 = document.createElement('p');\n    const p3 = document.createElement('p');\n    const p4 = document.createElement('p');\n    const p5 = document.createElement('p');\n    const p6 = document.createElement('p');\n\n    p1.textContent = 'No. 10, Roses Street';\n    p2.textContent = 'London';\n    p3.textContent = 'You could come by car, if you book a reservation by phone\\\n     , we have a free park place for you. You can reach us by tube, only 5 min \\\n     walk, or by bus which is very close.';\n    p4.textContent = 'Booking and reservations:';\n    p5.textContent = 'phone: 07453455555';\n    p6.textContent = 'rosesgarden@gmail.com';\n\n    p3.setAttribute('style', 'border-top: 5px solid red');\n    p6.setAttribute('style', 'color:red;');\n\n    adress1.append(h1, p1, p2, p3);\n    adress2.append(h2, p4, p5, p6);\n\nconst map = document.createElement('div');\n    map.setAttribute('id', 'map');\nconst img = new Image();\n    img.src = _map_jpg__WEBPACK_IMPORTED_MODULE_0__;\n    img.setAttribute('width', '400px');\n    map.appendChild(img);\n    adress.append(adress1, adress2, h3, map);\n\n    return contact;\n}\n\n//# sourceURL=webpack://restaurant-page/./src/contact.js?");
+
+/***/ }),
+
+/***/ "./src/map.jpg":
+/*!*********************!*\
+  !*** ./src/map.jpg ***!
+  \*********************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"a750b07bad242e0f1242.jpg\";\n\n//# sourceURL=webpack://restaurant-page/./src/map.jpg?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src;
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) {
+/******/ 					var i = scripts.length - 1;
+/******/ 					while (i > -1 && !scriptUrl) scriptUrl = scripts[i--].src;
+/******/ 				}
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/contact.js");
+/******/ 	
+/******/ })()
+;
---
+++ menu.bundle.js
@@ -0,0 +1,191 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/menu.js":
+/*!*********************!*\
+  !*** ./src/menu.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _breakfast_jpg__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./breakfast.jpg */ \"./src/breakfast.jpg\");\n/* harmony import */ var _fishandchips_jpg__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./fishandchips.jpg */ \"./src/fishandchips.jpg\");\n/* harmony import */ var _lambribs_jpg__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./lambribs.jpg */ \"./src/lambribs.jpg\");\n/* harmony import */ var _chikenwings_jpg__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./chikenwings.jpg */ \"./src/chikenwings.jpg\");\n/* harmony import */ var _pizza_jpg__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./pizza.jpg */ \"./src/pizza.jpg\");\n/* harmony import */ var _prowns_jpg__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./prowns.jpg */ \"./src/prowns.jpg\");\n/* harmony import */ var _wine1_jpg__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./wine1.jpg */ \"./src/wine1.jpg\");\n\n\n\n\n\n\n\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (menu);\n\n\nfunction menu(){\n        const menu_list = document.createElement('div');\n            menu_list.setAttribute('id', 'menu-list');\n        const menu = document.createElement('div');\n            menu.setAttribute('id', 'menu');\n            menu_list.appendChild(menu);\n        const div1 = document.createElement('h3');\n        const div2 = document.createElement('div');\n        const div3 = document.createElement('div');\n        const div4 = document.createElement('div');\n        const div5 = document.createElement('div');\n        const div6 = document.createElement('div');\n        const div7 = document.createElement('div');\n        const div8 = document.createElement('div');\n        div1.setAttribute('id', 'sugestion');\n        div1.textContent = 'Meal Sugestions';\n        const menu2 = document.createElement('div');\n        const menu3 = document.createElement('div');\n        const menu4 = document.createElement('div');\n        const menu5 = document.createElement('div');\n        const menu6 = document.createElement('div');\n        const menu7 = document.createElement('div');\n        const menu8 = document.createElement('div');\n\n        div2.appendChild(menu2);\n        div3.appendChild(menu3);\n        div4.appendChild(menu4);\n        div5.appendChild(menu5);\n        div6.appendChild(menu6);\n        div7.appendChild(menu7);\n        div8.appendChild(menu8);\n        \n        const p2 = document.createElement('p');\n        const p3 = document.createElement('p');\n        const p4 = document.createElement('p');\n        const p5 = document.createElement('p');\n        const p6 = document.createElement('p');\n        const p7 = document.createElement('p');\n        const p8 = document.createElement('p');\n\n        const h2 = document.createElement('h4');\n        const h3 = document.createElement('h4');\n        const h4 = document.createElement('h4');\n        const h5 = document.createElement('h4');\n        const h6 = document.createElement('h4');\n        const h7 = document.createElement('h4');\n        const h8 = document.createElement('h4');\n\n        menu2.appendChild(h2);\n        menu3.appendChild(h3);\n        menu4.appendChild(h4);\n        menu5.appendChild(h5);\n        menu6.appendChild(h6);\n        menu7.appendChild(h7);\n        menu8.appendChild(h8);\n        h2.textContent = 'English Breakfast';\n        h3.textContent = 'Fish and Chips ';\n        h4.textContent = 'Lamb Ribs';\n        h5.textContent = 'Chiken Wings';\n        h6.textContent = 'Pizza';\n        h7.textContent = 'Prowns Salat';\n        h8.textContent = 'Enjoy Drinks';\n    p2.textContent= 'bacon, fried egg, sausage, mushrooms, baked beans,toast\\n'  \n                    'hash brown and grilled tomatoes.';\n    p3.textContent= 'fish: cod, hadock, halibut with chips, green pease, tartare sauce, lemon';\n    p4.textContent= 'fresh baby lamb, rice, tzatziki sauce, hot pepper sauce, green salat';\n    p5.textContent= 'BBQ or Buffalo chicken bites, accompanied with blue cheese and stalks of celery.';\n    p6.textContent= 'with different topics: ham, salami, chees,\\n'\n                    'sliced tomat, spinach, red peppers and sliced mushrooms.';\n    p7.textContent= 'fride fresh prawns with lettuce, red onion, garlic, lemon and oliven oil';\n    p8.textContent= 'enjoy drinks from around the world, wine, spirits, beer, soft drinks and coktails';\n    \n    const img2 = new Image();\n    const img3 = new Image();\n    const img4 = new Image();\n    const img5 = new Image();\n    const img6 = new Image();\n    const img7 = new Image();\n    const img8 = new Image();\n    img2.setAttribute('height', '120px');\n    img3.setAttribute('height', '120px');\n    img4.setAttribute('height', '120px');\n    img5.setAttribute('height', '120px');\n    img6.setAttribute('height', '120px');\n    img7.setAttribute('height', '120px');\n    img8.setAttribute('height', '120px');\n\n    img2.src = _breakfast_jpg__WEBPACK_IMPORTED_MODULE_0__;\n    img3.src = _fishandchips_jpg__WEBPACK_IMPORTED_MODULE_1__;\n    img4.src = _lambribs_jpg__WEBPACK_IMPORTED_MODULE_2__;\n    img5.src = _chikenwings_jpg__WEBPACK_IMPORTED_MODULE_3__;\n    img6.src = _pizza_jpg__WEBPACK_IMPORTED_MODULE_4__;\n    img7.src = _prowns_jpg__WEBPACK_IMPORTED_MODULE_5__;\n    img8.src = _wine1_jpg__WEBPACK_IMPORTED_MODULE_6__;\n\n    menu2.appendChild(p2);\n    menu3.appendChild(p3);\n    menu4.appendChild(p4);\n    menu5.appendChild(p5);\n    menu6.appendChild(p6);\n    menu7.appendChild(p7);\n    menu8.appendChild(p8);\n\n    div2.appendChild(img2);\n    div3.appendChild(img3);\n    div4.appendChild(img4);\n    div5.appendChild(img5);\n    div6.appendChild(img6);\n    div7.appendChild(img7);\n    div8.appendChild(img8);\n\n    menu.append(div1, div2, div3, div4, div5, div6, div7, div8);\n        return menu_list;\n}\n\n\n//# sourceURL=webpack://restaurant-page/./src/menu.js?");
+
+/***/ }),
+
+/***/ "./src/breakfast.jpg":
+/*!***************************!*\
+  !*** ./src/breakfast.jpg ***!
+  \***************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"9b190a2ba689e01e81ea.jpg\";\n\n//# sourceURL=webpack://restaurant-page/./src/breakfast.jpg?");
+
+/***/ }),
+
+/***/ "./src/chikenwings.jpg":
+/*!*****************************!*\
+  !*** ./src/chikenwings.jpg ***!
+  \*****************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"d8a43c87b1106a01eaa8.jpg\";\n\n//# sourceURL=webpack://restaurant-page/./src/chikenwings.jpg?");
+
+/***/ }),
+
+/***/ "./src/fishandchips.jpg":
+/*!******************************!*\
+  !*** ./src/fishandchips.jpg ***!
+  \******************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"38de0acbdf0df098da48.jpg\";\n\n//# sourceURL=webpack://restaurant-page/./src/fishandchips.jpg?");
+
+/***/ }),
+
+/***/ "./src/lambribs.jpg":
+/*!**************************!*\
+  !*** ./src/lambribs.jpg ***!
+  \**************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"1668ba56508fda1c76c6.jpg\";\n\n//# sourceURL=webpack://restaurant-page/./src/lambribs.jpg?");
+
+/***/ }),
+
+/***/ "./src/pizza.jpg":
+/*!***********************!*\
+  !*** ./src/pizza.jpg ***!
+  \***********************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"6a16cbdee5e28c7e2d6e.jpg\";\n\n//# sourceURL=webpack://restaurant-page/./src/pizza.jpg?");
+
+/***/ }),
+
+/***/ "./src/prowns.jpg":
+/*!************************!*\
+  !*** ./src/prowns.jpg ***!
+  \************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"755f4f61f1bf23656d19.jpg\";\n\n//# sourceURL=webpack://restaurant-page/./src/prowns.jpg?");
+
+/***/ }),
+
+/***/ "./src/wine1.jpg":
+/*!***********************!*\
+  !*** ./src/wine1.jpg ***!
+  \***********************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"9bbe6a5502c75cb572a9.jpg\";\n\n//# sourceURL=webpack://restaurant-page/./src/wine1.jpg?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src;
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) {
+/******/ 					var i = scripts.length - 1;
+/******/ 					while (i > -1 && !scriptUrl) scriptUrl = scripts[i--].src;
+/******/ 				}
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/menu.js");
+/******/ 	
+/******/ })()
+;
